3c09d967bb61d8697a58c4578a7b0bd670183136
Emma Grey <emgrey02@gmail.com>
2026-06-15 17:09:02 -0400

    Work on factory functions

--- src/factories.js
+++ src/factories.js
@@ -1,53 +1,310 @@
-export const Ship = ({ length, timesHit = 0, sunk = false }) => {
+export const Ship = (length, name) => {
+  let _timesHit = 0;
+  let location = Array(length).fill(null);
+  let _hits = [];
+
+  const hit = (coord) => {
+    // return if already sunk
+    if (isSunk()) return;
+
+    // return if ship has already been hit there
+    let alreadyHitThere = _hits.find(
+      (hit) => hit.x == coord.x && hit.y == coord.y,
+    );
+    if (alreadyHitThere) return;
+
+    _timesHit++;
+    _hits.push(coord);
+  };
+
+  const isSunk = () => {
+    return _timesHit == length;
+  };
+
   return {
     length,
-    timesHit,
-    sunk,
-    location: Array({ length: length }).fill(null),
-    hit: () => {
-      timesHit++;
+    name,
+    get timesHit() {
+      return _timesHit;
     },
-    isSunk: () => {
-      if (timesHit >= length) {
-        sunk = true;
-        return true;
-      }
-      return false;
+    location,
+    get hits() {
+      return _hits;
     },
+    hit,
+    isSunk,
   };
 };
 
 export const Gameboard = () => {
   let board = Array.from({ length: 10 }, () => Array(10).fill(null));
-  return {
-    board,
-    missedShots: [],
-    placeShip: (ship, coordArr) => {
-      ship.location = coordArr;
-      coordArr.forEach((coord) => {
-        board[coord.x][coord.y] = ship;
+  let missedShots = [];
+
+  let ships = [
+    Ship(5, "carrier"),
+    Ship(4, "battleship"),
+    Ship(3, "destroyer"),
+    Ship(3, "submarine"),
+    Ship(2, "patrol boat"),
+  ];
+
+  const eraseBoard = () => {
+    board.forEach((row, rowNum) => {
+      row.forEach((col, colNum) => {
+        board[rowNum][colNum] = null;
       });
-    },
-    // coords object is { x: num, y: num }
-    receiveAttack: (coords) => {
-      board[coords.x][coords.y]
-        ? board[coords.x][coords.y].hit()
-        : missedShots.push(coords);
-    },
+    });
+  };
+
+  const getRandomCoords = (length, board) => {
+    let optionsArray = [];
+
+    const checkDirection = (dir, loc = { x: 0, y: 0 }, length) => {
+      let validCells = [];
+
+      switch (dir) {
+        case "left":
+          if (loc.y >= length - 1) {
+            for (let i = 0; i < length; i++) {
+              if (!board[loc.x][loc.y - i]) {
+                const coord = { x: loc.x, y: loc.y - i };
+                if (checkSurroundingCells(board, coord)) {
+                  validCells.push(coord);
+                } else {
+                  return false;
+                }
+              } else {
+                return false;
+              }
+            }
+            return validCells;
+          }
+          return false;
+          break;
+        case "right":
+          if (loc.y <= 9 - (length - 1)) {
+            for (let i = 0; i < length; i++) {
+              if (!board[loc.x][loc.y + i]) {
+                const coord = { x: loc.x, y: loc.y + i };
+                if (checkSurroundingCells(board, coord)) {
+                  validCells.push(coord);
+                } else {
+                  return false;
+                }
+              } else {
+                return false;
+              }
+            }
+            return validCells;
+          }
+          return false;
+          break;
+        case "up":
+          if (loc.x >= length - 1) {
+            for (let i = 0; i < length; i++) {
+              if (!board[loc.x - i][loc.y]) {
+                const coord = { x: loc.x - i, y: loc.y };
+                if (checkSurroundingCells(board, coord)) {
+                  validCells.push(coord);
+                } else {
+                  return false;
+                }
+              } else {
+                return false;
+              }
+            }
+            return validCells;
+          }
+          return false;
+          break;
+        case "down":
+          if (loc.x <= 9 - (length - 1)) {
+            for (let i = 0; i < length; i++) {
+              if (!board[loc.x + i][loc.y]) {
+                const coord = { x: loc.x + i, y: loc.y };
+                if (checkSurroundingCells(board, coord)) {
+                  validCells.push(coord);
+                } else {
+                  return false;
+                }
+              } else {
+                return false;
+              }
+            }
+            return validCells;
+          }
+          return false;
+          break;
+        default:
+          break;
+      }
+    };
+
+    board.forEach((row, rowNum) => {
+      row.forEach((cell, cellNum) => {
+        if (cell == null) {
+          // its a possibility, check directions starting from that cell
+          const currLoc = { x: rowNum, y: cellNum };
+          let leftRes = checkDirection("left", currLoc, length);
+          leftRes && optionsArray.push(leftRes);
+          let rightRes = checkDirection("right", currLoc, length);
+          rightRes && optionsArray.push(rightRes);
+          let upRes = checkDirection("up", currLoc, length);
+          upRes && optionsArray.push(upRes);
+          let downRes = checkDirection("down", currLoc, length);
+          downRes && optionsArray.push(downRes);
+        }
+      });
+    });
+    //console.log(optionsArray);
+    return optionsArray[Math.floor(Math.random() * optionsArray.length)];
+  };
+
+  const checkSurroundingCells = (board, coord) => {
+    let validMatrix = [
+      [false, false, false],
+      [false, true, false],
+      [false, false, false],
+    ];
+
+    if (coord.x == 0) {
+      // top line clear
+      validMatrix[0][0] = true;
+      validMatrix[0][1] = true;
+      validMatrix[0][2] = true;
+    } else if (!board[coord.x - 1][coord.y]) {
+      // cell above is null / open
+      validMatrix[0][1] = true;
+
+      //top left
+      if (coord.y - 1 >= 0) {
+        if (!board[coord.x - 1][coord.y - 1]) {
+          validMatrix[0][0] = true;
+        } else {
+          return false;
+        }
+      }
+
+      //top right
+      if (coord.y + 1 <= 9) {
+        if (!board[coord.x - 1][coord.y + 1]) {
+          validMatrix[0][2] = true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    if (coord.y == 0) {
+      // left line clear
+      validMatrix[0][0] = true;
+      validMatrix[1][0] = true;
+      validMatrix[2][0] = true;
+    } else if (!board[coord.x][coord.y - 1]) {
+      // cell to the left is null / open
+      validMatrix[1][0] = true;
+
+      //bottom left
+      if (coord.x + 1 < 9) {
+        if (!board[coord.x + 1][coord.y - 1]) {
+          validMatrix[2][0] = true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    if (coord.x == 9) {
+      // bottom line clear
+      validMatrix[2][0] = true;
+      validMatrix[2][1] = true;
+      validMatrix[2][2] = true;
+    } else if (!board[coord.x + 1][coord.y]) {
+      // cell directly underneath is null / open
+      validMatrix[2][1] = true;
+
+      // bottom right
+      if (coord.y + 1 < 9) {
+        if (!board[coord.x + 1][coord.y + 1]) {
+          validMatrix[2][2] = true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    if (coord.y == 9) {
+      //right line clear
+      validMatrix[0][2] = true;
+      validMatrix[1][2] = true;
+      validMatrix[2][2] = true;
+    } else if (!board[coord.x][coord.y + 1]) {
+      // cell directly to the right is null / open
+      validMatrix[1][2] = true;
+
+      // top right
+      if (coord.x - 1 > 0) {
+        if (!board[coord.x - 1][coord.y + 1]) {
+          validMatrix[0][2] = true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    return true;
+  };
+
+  const placeShip = (ship, coordArr = null) => {
+    if (!coordArr) {
+      coordArr = getRandomCoords(ship.length, board);
+    }
+    ship.location = coordArr;
+    coordArr.forEach((coord) => {
+      board[coord.x][coord.y] = ship;
+    });
+  };
+
+  // coords object is { x: num, y: num }
+  const receiveAttack = (coord) => {
+    if (board[coord.x][coord.y]) {
+      // a ship is in this location
+      board[coord.x][coord.y].hit(coord);
+      return true;
+    } else {
+      missedShots.push(coord);
+      return false;
+    }
+  };
+
+  const checkSunkShips = () => {
+    return ships.every((ship) => {
+      return ship.isSunk();
+    });
+  };
+
+  return {
+    ships,
+    board,
+    missedShots,
+    placeShip,
+    receiveAttack,
+    checkSunkShips,
+    eraseBoard,
   };
 };
 
-export const Player = ({ type }) => {
+export const Player = (type) => {
   return {
     type,
     gameboard: Gameboard(),
   };
 };
-
-let player = Player({ type: "real" });
-console.log(player.gameboard.board);
-player.gameboard.placeShip(Ship({ length: 3 }), [
-  { x: 0, y: 0 },
-  { x: 1, y: 0 },
-]);
-console.log(player.gameboard.board);
